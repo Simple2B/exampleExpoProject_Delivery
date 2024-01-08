2e6c7b59b3929d263127630ad0910ad3a283f426
Varvara Shcherbyna <varvarashcherbyna7@gmail.com>
2024-01-08 17:39:10 +0200

    google auth cred for ios and android

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,14 +3,21 @@ import {useEffect} from 'react';
 import * as SplashScreen from 'expo-splash-screen';
 import {QueryClientProvider, QueryClient} from '@tanstack/react-query';
 import {useFonts} from 'expo-font';
+import Constants from 'expo-constants';
+import * as SecureStore from 'expo-secure-store';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import {DarkTheme, DefaultTheme, ThemeProvider} from '@react-navigation/native';
 import {BottomSheetModalProvider} from '@gorhom/bottom-sheet';
 import {ScreenName} from '@/constants/screens/screens';
-import Constants from 'expo-constants';
 import StackScreens from '@/navigation/StackScreens';
+import {StorageKeys} from '@/constants/storage';
 
 const queryClient = new QueryClient();
+
+// OpenAPI configuration
+// OpenAPI.BASE = process.env.EXPO_PUBLIC_API_URL ?? '';
+// OpenAPI.TOKEN = async () =>
+//   (await SecureStore.getItemAsync(StorageKeys.TOKEN)) ?? '';
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
--- components/GoogleAppleLogin/GoogleLogin.tsx
+++ components/GoogleAppleLogin/GoogleLogin.tsx
@@ -1,3 +1,7 @@
+// web 1000274289123-p5b1sjcbpi41s0kvnl01njumaf604sp5.apps.googleusercontent.com
+// ios 1000274289123-1nhairjivuhuv5ig30t1nk5tqribf9qq.apps.googleusercontent.com
+// android 1000274289123-3lj5ojsqa3b0j9pjadl0cdgdtmhmb2ss.apps.googleusercontent.com
+
 import {TouchableOpacity, Image} from 'react-native';
 import React from 'react';
 import images from '@/assets/images';
