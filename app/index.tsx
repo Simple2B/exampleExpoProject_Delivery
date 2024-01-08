c8c4d283f06db63b3bcde69411bf5c16ec418e52
sa1am8 <tinkerino571@gmail.com>
2024-01-08 14:40:05 +0200

    fix

--- app/index.tsx
+++ app/index.tsx
@@ -4,6 +4,12 @@ import { useFonts } from "expo-font";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { colors } from "@/constants/colors";
 
+import {OpenAPI} from "../src/services";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+OpenAPI.BASE = process.env.REACT_APP_MY_OPEN_API_URL ?? 'https://api.delivery.simple2b.net';
+OpenAPI.TOKEN = async () => (await AsyncStorage.getItem("userToken")) ?? '';
+
 const Welcome = () => {
   const [fontsLoaded] = useFonts({
     "Baloo Bhai 2": require("../assets/fonts/BalooBhai2-Regular.ttf"),
